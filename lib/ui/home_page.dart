47e8dac2413b718027ee6b6b5bdc91ca3946b0e4
Guilherme <guilhermekoerich@gmail.com>
2020-02-22 15:01:52 -0300

    First part completed

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String _search;
+  int _offset = 0;
+
+  Future<Map>_getGifs() async {
+    http.Response response;
+
+    print(_isSerach());
+
+    if(_isSerach()) {
+      response = await http.get('https://api.giphy.com/v1/gifs/trending?api_key=lC2DfQEDDZRats6Ng4kjI8XRvoUQOV23&limit=20&rating=G');
+    } else {
+      response = await http.get('https://api.giphy.com/v1/gifs/search?api_key=lC2DfQEDDZRats6Ng4kjI8XRvoUQOV23&q=$_search&limit=19&offset=$_offset&rating=G&lang=en');
+    }
+
+    return json.decode(response.body);
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map) { print(map); });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network('https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif'),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(11.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Pesquise aqui!',
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder()
+              ),
+            style: TextStyle(color: Colors.white, fontSize: 18.0),
+            textAlign: TextAlign.center,
+            onSubmitted: (text) {
+              setState(() {
+                this._search = text;
+                this._offset = 0;
+              });
+            },
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch(snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 250.0,
+                      height: 250.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if(snapshot.hasError) {
+                      return Container();
+                    }
+
+                    return _createGifTable(context, snapshot);
+                }
+              }
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool _isSerach() {
+    return this._search == null || this._search == '';
+  }
+
+  int _getCount(List data) {
+    if(_isSerach()) {
+      return data.length;
+    }
+
+    return data.length + 1;
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: EdgeInsets.all(11.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 11.0,
+        mainAxisSpacing: 11.0,
+      ),
+      itemCount: _getCount(snapshot.data['data']),
+      itemBuilder: (context, index) {
+        if(_isSerach() || index < snapshot.data['data'].length) {
+          return GestureDetector(
+            child: Image.network(
+              snapshot.data['data'][index]['images']['fixed_height']['url'],
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+          );
+        } else {
+          return Container(
+            child: GestureDetector(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.add, color: Colors.white, size: 70.0,),
+                  Text('Carregar mais...', style: TextStyle(color: Colors.white, fontSize: 20.0),)
+                ],
+              ),
+              onTap: () {
+                setState(() {
+                  this._offset += 19;
+                });
+              },
+            ),
+          );
+        }
+      }
+    );
+  }
+
+}
