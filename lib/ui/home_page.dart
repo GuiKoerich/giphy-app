b2609ead2fd44c24456dfc5fa6daab5c0274b46d
Guilherme <guilhermekoerich@gmail.com>
2020-02-22 15:32:40 -0300

    Add share gif

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:giphy/ui/gif_page.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
 import 'dart:async';
 import 'dart:convert';
 
@@ -130,6 +131,9 @@ class _HomePageState extends State<HomePage> {
               Navigator.push(context, 
                 MaterialPageRoute(builder: (context) => GifPage(snapshot.data['data'][index]))
               );
+            },
+            onLongPress: () {
+              Share.share(snapshot.data['data'][index]['images']['fixed_height']['url']);
             },
           );
         } else {
