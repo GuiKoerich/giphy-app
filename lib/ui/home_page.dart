2e8d1c632de64e37fa2cb8ab10dd926795e6cd9b
Guilherme <guilhermekoerich@gmail.com>
2020-02-22 15:40:46 -0300

    Add faded transition gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:giphy/ui/gif_page.dart';
 import 'package:http/http.dart' as http;
 import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
 import 'dart:async';
 import 'dart:convert';
 
@@ -122,8 +123,9 @@ class _HomePageState extends State<HomePage> {
       itemBuilder: (context, index) {
         if(_isSerach() || index < snapshot.data['data'].length) {
           return GestureDetector(
-            child: Image.network(
-              snapshot.data['data'][index]['images']['fixed_height']['url'],
+            child: FadeInImage.memoryNetwork(
+              placeholder: kTransparentImage, 
+              image: snapshot.data['data'][index]['images']['fixed_height']['url'],
               height: 300.0,
               fit: BoxFit.cover,
             ),
